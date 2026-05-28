b28ea7c1a75d7f3a2190156b474cd9180136a716
SAR Developer <sardeveloper@gmail.com>
2026-05-28 21:43:47 +0200

    Add Google Analytics

--- server.js
+++ server.js
@@ -109,6 +109,8 @@ app.post("/tts", requireLogin, (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("🚀 VoicePro running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 SAR Text to Speech running on port", PORT);
 });
